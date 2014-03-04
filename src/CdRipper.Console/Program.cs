0c59d55f67fc3c0893ba36262ac91cc4f2b8e00d
Lodewijk Sioen <lodewijksioen@gmail.com>
2014-03-04 06:18:53 +0100

    Extracted method before trying MusicBrainz

--- src/CdRipper.Console/Program.cs
+++ src/CdRipper.Console/Program.cs
@@ -14,50 +14,57 @@ namespace CdRipper.TestConsole
             while (true)
             {
                 Console.WriteLine("enter the driveletter to read or ctrl-c to quit");
-                var driveletter = Console.ReadKey();
+                var driveletter = Console.ReadKey().KeyChar.ToString();
                 Console.WriteLine();
                 try
                 {
-                    using (var drive = new CdDrive(driveletter.KeyChar.ToString()))
-                    {
-                        if (!drive.IsCdInDrive())
-                        {
-                            Console.WriteLine("No CD in drive");
-                            continue;
-                        }
-
-                        var toc = drive.ReadTableOfContents();
-                        Console.WriteLine("number of tracks:" + toc.Tracks.Count);
-                        Console.WriteLine("CDDB id:" + FreeDbDiscIdCalculator.CalculateDiscId(toc));
-                        foreach (var track in toc.Tracks)
-                        {
-                            Console.WriteLine("track {0}: lenth={1}-{2}", track.TrackNumber, track.StartSector, track.EndSector);
-                        }
-                        Console.WriteLine("Enter tracknumber to rip");
-                        var trackNumber = Convert.ToInt32(Console.ReadLine());
-                        using (var trackReader = new TrackReader(drive))
-                        {
-                            using (var encoder = new LameMp3Encoder(new EncoderSettings
-                            {
-                                OutputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), String.Format(@"encoding\track{0:##}.mp3", trackNumber))
-                            }))
-                            {
-                                trackReader.ReadTrack(toc.Tracks.First(t => t.TrackNumber == trackNumber),
-                                    b =>
-                                    {
-                                        encoder.Write(b);
-                                    },
-                                    (i, a) =>
-                                    {
-                                        Console.WriteLine("{0} of {1} read", i, a);
-                                    });
-                            }
-                        }
-                    }
+                    RipWithNativeMethods(driveletter);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        
+
+        static void RipWithNativeMethods(string driveletter)
+        {
+            using (var drive = new CdDrive(driveletter))
+            {
+                if (!drive.IsCdInDrive())
+                {
+                    Console.WriteLine("No CD in drive");
+                    return;
+                }
+
+                var toc = drive.ReadTableOfContents();
+                Console.WriteLine("number of tracks:" + toc.Tracks.Count);
+                Console.WriteLine("CDDB id:" + FreeDbDiscIdCalculator.CalculateDiscId(toc));
+                foreach (var track in toc.Tracks)
+                {
+                    Console.WriteLine("track {0}: lenth={1}-{2}", track.TrackNumber, track.StartSector, track.EndSector);
+                }
+                Console.WriteLine("Enter tracknumber to rip");
+                var trackNumber = Convert.ToInt32(Console.ReadLine());
+                using (var trackReader = new TrackReader(drive))
+                {
+                    using (var encoder = new LameMp3Encoder(new EncoderSettings
+                    {
+                        OutputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), String.Format(@"encoding\track{0:##}.mp3", trackNumber))
+                    }))
+                    {
+                        trackReader.ReadTrack(toc.Tracks.First(t => t.TrackNumber == trackNumber),
+                            b =>
+                            {
+                                encoder.Write(b);
+                            },
+                            (i, a) =>
+                            {
+                                Console.WriteLine("{0} of {1} read", i, a);
+                            });
+                    }
                 }
             }
         }
